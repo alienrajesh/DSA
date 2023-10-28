aeafd781beb32f719f8f944deb7d97b89ca74f14
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-10-29 00:04:38 +0530

    leetcode 0347.py done today

--- 0347.py
+++ 0347.py
@@ -1,18 +1,31 @@
 
-
 # Input: nums = [1,1,1,2,2,3], k = 2
 # Output: [1,2]
 
 
 nums = [1,1,1,2,2,3]
+k = 2
 
-
+# O(n) - time complexity 
+# O(n) - space complexity
 def topKFrequent(nums: list[int], k: int = None) -> list[int]:
     
-    hashMap = {}
-    for t in nums:
-        hashMap[t] = 1 + hashMap.get(t,0)
-    print(hashMap)   
+    count = {}
+    freq = [[] for i in range(len(nums) +1 )]
     
-topKFrequent(nums) 
+    for n in nums:
+        count[n] = 1 + count.get( n,0)
+    
+    for n , c in count.items():
+        freq[c].append(n)
+        
+    res = []
+    
+    for i in range(len(freq)-1, 0 ,-1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+    
+topKFrequent(nums,k) 
     
