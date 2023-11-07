af7c2ca63910a3ecccf7728e696393ef78b153da
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-11-07 12:26:30 +0530

    Contains duplicate 1

--- 0217.py
+++ 0217.py
@@ -1,12 +1,31 @@
 
 
-nums = [1,2,3,4] 
+nums = [1,2,3,1] 
 
 
 def check(nums):
-    hashset = set()
-    for n in nums:
-        if n in hashset:
-            return True
-        hashset.add(n)
+    hashSet = set()
+    for i in nums:
+        if i in hashSet:
+            return True 
+        hashSet.add(i)
     return False
+        
+        
+
+
+#another method is to check the length of hashSet and nums
+# if len(hashSet) != len(nums) then we return false
+
+
+#diff method with dictionary
+
+def check2(nums):
+    numbers = {}
+    for i in nums:
+        if i in numbers:
+            return True
+        numbers[i] = 1
+    return False
+
+print(check2(nums))
