08e0a29e41114489ca596679e584b3391b37a763
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-11-08 19:25:28 +0530

    Longest consecutive sequence

---
+++ 0128.py
@@ -0,0 +1,38 @@
+#longest consecutive sequence
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is
+#              [1, 2, 3, 4]. Therefore its length is 4.
+
+# [1,2,3,4,100,200]
+# [ 1, 3 , 4, 5, 6, 100, 200]
+
+nums =  [ 1, 3 , 4, 5, 6, 100, 101, 102, 103, 104, 105, 200]
+
+def longestConsecutive(nums: list[int]) -> int:
+    
+    nums.sort()
+    print(nums)
+    max = 0 
+    counter = 0
+    for i in range(len(nums)-1):
+        if nums[i+1] - nums[i] == 1 :
+            counter +=1
+        else:
+            counter = 0
+            
+        if max < counter :
+            max = counter
+        
+                  
+    return max + 1
+           
+       
+        
+        
+    
+    
+    
+    
+    
+print(longestConsecutive(nums))    
