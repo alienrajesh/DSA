45d196a18c1f12037439063ec89d06e9c5dd9c28
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-11-14 20:53:57 +0530

    3sum

--- 0015.py
+++ 0015.py
@@ -13,26 +13,37 @@
 nums = [-1,0,1,2,-1,-4]
 
 
-def threeSum(nums: list[int]) -> list[list[int]]:
-    l , r = 0 , len(nums) - 1
-    
+def three3Sum(nums: list[int]) -> list[list[int]]:
     
     res = [ ]
-    while l < r:
-        m = (l+r) //2
-        
-        if nums[l] + nums[m] + nums[r] == 0:
-            res.append([nums[l], nums[m] , nums[r]]) 
-            
-            
-        while  nums[l] + nums[m] + nums[r] != 0 :
-            l+=1
-        
-        
-    return res    
-        
     
+    nums.sort()
+    
+    for i , a in enumerate(nums):
+        
+        if i > 0 and a == nums[i-1]:
+            continue
+        
+        l , r = i+1 , len(nums) - 1
+        
+        while l < r :
+            threesum = a + nums[l] + nums[r]
+            
+            if threesum > 0 :
+                r-=1
+            elif threesum < 0:
+                l+=1
+            else:
+                res.append([a, nums[l] , nums[r]]) 
+
+                l+=1
+                r-=1
+                while nums[l] == nums[l-1] and l < r:
+                    l +=1
+        
+    return res            
   
-print(threeSum(nums))      
+  
+print(three3Sum(nums))      
     
     
