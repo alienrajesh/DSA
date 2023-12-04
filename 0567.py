9677bc51148c77212dd4e014619f5aa3b5404bd1
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-04 19:12:41 +0530

    Permutation in String

--- 0567.py
+++ 0567.py
@@ -33,4 +33,55 @@ def checkInclusion( s1: str, s2: str) -> bool:
             return False
     return True
 
-print(checkInclusion(s1,s2))    
+# print(checkInclusion(s1,s2))    
+
+
+
+# M 2 : might sort both of them and check if presen in there : XXX wrong XXX
+
+
+
+
+# M 3 : a window the size of s1 and track if all elements are in that window 
+
+
+def checkInclusion2( s1: str, s2: str) -> bool:
+    if len(s1) > len(s2) : 
+        return False
+
+    s1count , s2count = [0]*26 , [0]*26
+    
+    for i in range(len(s1)):
+        s1count[ord(s1[i])- ord("a")] += 1
+        s2count[ord(s2[i])- ord("a")] += 1
+    
+    matches = 0
+    for i in range(26):
+        matches += 1 if s1count[i] == s2count[i] else 0
+    l = 0
+    for r in range(len(s1),len(s2)):
+        if matches == 26:
+            return True
+        
+        index = ord(s2[r]) - ord("a")
+        
+        s2count[index] +=1
+        
+        if s1count[index] == s2count[index] :
+            matches +=1
+        elif s1count[index] +1 == s2count[index] :
+            matches -=1
+        
+        index = ord(s2[l]) - ord("a")
+        s2count[index] -=1
+        
+        if s1count[index] == s2count[index] :
+            matches +=1
+        elif s1count[index] - 1 == s2count[index] :
+            matches -=1
+        
+        l +=1
+        
+    return matches == 26
+
+print(checkInclusion2(s1 , s2) )            
