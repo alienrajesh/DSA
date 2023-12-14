870f1a923f711c728ed8c7961113b1f0ccb3eb2f
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-14 22:45:24 +0530

    Sliding Window Maximum

--- 0239.py
+++ 0239.py
@@ -1,6 +1,6 @@
 # NEETCODE Topic - Sliding Window
 
-
+from collections import deque
 # 239. Sliding Window Maximum
 
 # Example 1:
@@ -44,15 +44,47 @@ def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
     while r < len(nums):
         window.append(nums[r])
         window.remove(nums[l])
-        res.append(max(window))
+        if nums[r] > res[l] :
+            res.append(nums[r])
+        else:
+            res.append(max(window))
         print(f"Window at step{l} {window}")
         l+=1
         r+=1
     return res   
-        
-        
-
-     
-        
+    
 print(maxSlidingWindow(nums,k))
 
+
+## new approach this might take less time
+# instead of checking the window everytime we can check the new element added is
+# greater than the max of the current window 
+
+
+
+
+## Neetcode solution 
+# time - O(n) 
+# space - O(n)
+
+def maxSlidingWindow(nums:list[int],k:int) -> list[int]:
+    res = []
+    q = deque()
+    l = r = 0
+    while r < len(nums) :
+        # pop smaller values from q 
+        while q and nums[q[-1]] < nums[r] :
+            q.pop()
+        q.append(r)
+        
+        # remove left val from window 
+        if l > q[0] :
+            q.popleft()
+        
+        if (r+1) >= k:
+            res.append(nums[q[0]])
+            l+=1
+        r +=1
+    
+    return res
+    
