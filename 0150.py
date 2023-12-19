94b9e93a2dfbb8f66cd354b2d16e5976355f6a46
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-19 19:02:23 +0530

    Evaluate Reverse Polish Notation

--- 0150.py
+++ 0150.py
@@ -30,12 +30,24 @@
 # = 22
 
 
-tokens = ["2","1","+","3","*"]
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
 
-def evalRPN( tokens: list[str]) -> int:
+def evalRPN(tokens: list[str]) -> int:
+    stack = []
+    for i in tokens:
+        if i == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif i == "-":
+            a , b = stack.pop() , stack.pop()
+            stack.append(b-a)
+        elif i == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif i == "/":
+            a , b = stack.pop() , stack.pop() 
+            stack.append(int(b / a))
+        else:
+            stack.append(int(i))       
     
-    operations = 0
-    for i in range(len(tokens)-1, -1, -1):
-        print(tokens[i])
-        
-evalRPN(tokens)        
+    return stack[0]
+
+print(evalRPN(tokens))        
