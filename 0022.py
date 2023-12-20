21ed3c236d63c44cf9d602135f63cb16007ac26a
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-20 21:38:39 +0530

    Generate Parenthesis

---
+++ 0022.py
@@ -0,0 +1,14 @@
+
+# Generate Parenthesis
+
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Example 2:
+
+# Input: n = 1
+# Output: ["()"]
+
+def generateParenthesis(n: int) -> list[str]:
