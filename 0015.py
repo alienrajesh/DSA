e0c4da9df923bf3c6f4fc1a3e57e9d6e9bd33173
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-23 21:01:52 +0530

    Three Sum

--- 0015.py
+++ 0015.py
@@ -13,7 +13,7 @@
 nums = [-1,0,1,2,-1,-4]
 
 
-def three3Sum(nums: list[int]) -> list[list[int]]:
+def threeSum(nums: list[int]) -> list[list[int]]:
     
     res = [ ]
     
@@ -27,11 +27,11 @@ def three3Sum(nums: list[int]) -> list[list[int]]:
         l , r = i+1 , len(nums) - 1
         
         while l < r :
-            threesum = a + nums[l] + nums[r]
+            sum = a + nums[l] + nums[r]
             
-            if threesum > 0 :
+            if sum > 0 :
                 r-=1
-            elif threesum < 0:
+            elif sum < 0:
                 l+=1
             else:
                 res.append([a, nums[l] , nums[r]]) 
@@ -44,6 +44,128 @@ def three3Sum(nums: list[int]) -> list[list[int]]:
     return res            
   
   
-print(three3Sum(nums))      
+# print(threeSum(nums))      
+    
+
+
+def threesum(nums: list[int]) -> list[list[int]]:
+     
+    res = [] 
+    
+    nums.sort() 
+    
+    for i , a in enumerate(nums):
+        # skip positive integers
+        if a > 0 :
+            break
+        
+        
+        if i > 0 and a == nums[i-1]:
+            continue
+        
+        l , r = i +1 , len(nums) -1
+        
+        while l < r :
+            
+            sum = a + nums[l] + nums[r] 
+            
+            if  sum < 0 :
+                l+=1
+            elif sum>0 :
+                r-=1
+            else:
+                res.append([a, nums[l] , nums[r]])
+                l+=1
+                r-=1
+                
+                while nums[l] == nums[l-1] and l<r :
+                    l+=1
+                    
+    return res
+
+# print(threesum(nums))
+
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+
+            
+def threesum2(nums: list[int]) -> list[list[int]]:
+      
+    res = []
+    nums.sort()
     
     
+    for  i , a in enumerate(nums):
+        #skips +ve int
+        if a > 0 :
+            break 
+        
+        #checks for same element
+        if i > 0 and a == nums[i-1]:
+            continue
+        
+        l , r = i +1 , len(nums) -1       # [-1, 0, 1, 2, -1, -4]
+                                          #   i  l             r
+        while l < r :
+            
+            sum = a + nums[l] + nums[r]
+            
+            if sum > 0 :
+                r-=1
+            elif sum < 0:
+                l +=1
+            else :
+                res.append([a,nums[l] , nums[r]])
+                
+                l += 1
+                r -= 1
+
+                while l < r and nums[l] == nums[l-1]:
+                    l+=1
+    print(res)
+
+            
+threesum2(nums )            
+            
+            
+            
+            
+def threesum3(nums:list[int]) -> list[list[int]]:
+    
+    res = []
+    nums.sort()
+
+    
+    
+    for i , a in enumerate(nums) :
+        
+        if a > 0 :
+            break
+        
+        if i > 0 and a == nums[i-1] :
+            continue
+        
+        l , r = i + 1 , len(nums)-1
+        
+        while l < r : 
+        
+            sum = a + nums[l] + nums[r]
+
+            if sum > 0 :
+                r-=1
+            elif sum < 0 :
+                l+=1
+
+            else : 
+                res.append([i,nums[l],nums[r]])
+                
+                l+=1
+                r-=1
+                
+                while l < r and nums[l] == nums[l-r] : 
+                    l+=1
+    return res
+    
+print(threesum3(nums) )   
