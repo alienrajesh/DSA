406976e31c999f85a80b718b3691ce52c1a54391
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2023-12-26 00:01:58 +0530

    Generate Parenthesis

--- 0022.py
+++ 0022.py
@@ -12,3 +12,30 @@
 # Output: ["()"]
 
 def generateParenthesis(n: int) -> list[str]:
+    
+    
+    stack = []
+    res = []
+    
+    def backtrack(openN,closeN):
+        if openN == closeN == n:
+            res.append("".join(stack))
+            return 
+            
+        if openN < n:
+            stack.append("(")
+            backtrack(openN +1,closeN)
+            stack.pop()
+        
+        if closeN < openN:
+            stack.append(")")
+            backtrack(openN , closeN +1)
+            stack.pop()
+            
+    backtrack(0,0)
+    
+    return res
+
+n = 3
+
+print(generateParenthesis(n))
