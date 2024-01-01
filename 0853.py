8dd160e97056a31a396c34f27cfce3aa01435c6b
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-01-01 19:36:26 +0530

    Car Fleet

--- 0853.py
+++ 0853.py
@@ -56,22 +56,21 @@ def carFleet(target: int,position: list[int], speed: list[int]) -> int:
    
 
 ## NEETCODE solution
-
+# time - O(nlogn) coz of sorting
+# space - O(n)
 
 def carFleet2(target:int, position : list[int] , speed: list[int]) -> int:
     
     pair = [[p,s] for p,s in zip(position,speed)]
-    
-    pair.sort(reverse=True)
     stack = []
-    
+    pair.sort(reverse=True) 
+    print(pair)   
     for p,s in pair:
         stack.append((target-p)/s)
         
         if len(stack) >= 2 and stack[-1] <= stack[-2]:
             stack.pop()
-            
-    return len(stack)
     
+    return len(stack)
     
 print(carFleet2(12, [10,8,0,5,3],  [2,4,1,1,3]))
