95e18cc512f5a4ab7ac2e4f778c19824fdeeecb4
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-01-10 19:00:59 +0530

    KOKO Eating Banana

--- 0875.py
+++ 0875.py
@@ -23,8 +23,12 @@
 
 from math import ceil
 
-piles = [30,11,23,4,20]
-h = 6
+piles = [332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
+h = 823855818
+
+# Brute Force solution 
+# time - O(n^2)
+# space - O(1)
 
 def minEatingSpeed(piles:list[int] , h : int) -> int:
     
@@ -37,6 +41,28 @@ def minEatingSpeed(piles:list[int] , h : int) -> int:
         print(f"time-{time}")    
         if time <= h:
             return k
-      
+
+
+
+def minEatingSpeed2(piles:list[int],h:int) -> int:
+    
+    l , r = 1 , max(piles)
+    
+    while l <= r :
+        speed = round((l+r)/2)
+        time = 0
+        for p in piles :
+            time += ceil(p/speed)
+        
+        if time < h :
+            r = speed -1 
+        elif time > h:
+            l = speed + 1 
+        if time == h :
+            return speed
+    
+    return "not found"
+
+              
 print(minEatingSpeed(piles,h))
 
