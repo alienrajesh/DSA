fd782064ba8c0d2d3de9dc3fba40442a31e58247
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-01-27 18:11:59 +0530

    Search In Roatated Sorted Array

--- 0033.py
+++ 0033.py
@@ -20,43 +20,42 @@
 # TestCase - nums = [5,1,2,3,4] , target = 1 , output = 1
 
 
-# Brute force 
+# Brute force
 # Time - O(N)
 # Space - O(1)
 
-nums =[5,1,2,3,4]
+nums = [5, 1, 2, 3, 4]
 target = 1
 
-def search(nums:list[int],target) -> int:
-    
+
+def search(nums: list[int], target) -> int:
     for r in range(len(nums)):
         if nums[r] == target:
-            return r 
+            return r
     return -1
 
+
 def search2(nums: list[int], target: int) -> int:
-    
-    l , r = 0 , len(nums)-1
-    
-    while l<=r :
-        m = (l+r) // 2 
-        if nums[m] == target :
-            return m 
-        
-        # left sorted portion 
-        if nums[l] <= nums[m] :
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            return m
+        # left sorted portion
+        if nums[l] <= nums[m]:
             if target > nums[m] or target < nums[l]:
-                l = m +1
+                l = m + 1
             else:
-                r = m -1
+                r = m - 1
         # right sorted portion
         else:
             if target < nums[m] or target > nums[r]:
-                r = m - 1 
-            else: 
+                r = m - 1
+            else:
                 l = m + 1
-        
+
     return -1
 
 
-print(search2(nums,target))
+print(search2(nums, target))
