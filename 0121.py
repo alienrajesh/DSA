7726e11657aa24b439cd61a28bf644039e0eb97b
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-02-04 18:59:31 +0530

    Best Time to Buy and Sell Stock

--- 0121.py
+++ 0121.py
@@ -82,6 +82,23 @@ print(maxProfit3(prices))
 
 
 
-
-
-
+def maxProfit4(prices:list[int]) -> int:
+    
+    l  = 0 
+    r = 1
+    res = 0
+    
+    while r < len(prices):
+        if prices[l] < prices[r] :
+            profit = prices[r] - prices[l]
+            res = max(res, profit)
+        
+        
+        else :
+            l = r
+        
+        r +=1
+    return res         
+    
+    
+print(maxProfit4(prices))
