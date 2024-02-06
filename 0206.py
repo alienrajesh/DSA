f71569908af5ce28cd5b003634f81e3a63400f6f
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-02-06 17:52:02 +0530

    Reversed Linked list

--- 0206.py
+++ 0206.py
@@ -1,4 +1,4 @@
-
+# leetcode 0206.py "Reversed Linked list"
 
 # Example 1:
 
@@ -20,7 +20,7 @@
 
 # Method 1:
 # iterating every object in the given linked list and making another linkedlist
-# inserting at the begining of the newLinkedList 
+# inserting at the begining of the newLinkedList
 
 # # Definition for singly-linked list.
 # class ListNode:
@@ -33,13 +33,13 @@
 
 #         if head == None or head.next == None :
 #             return head
-        
+
 
 #         itr = head
-#         root = LinkedList() 
+#         root = LinkedList()
 #         while itr :
 #             root.insert_at_begining(itr.val)
-#             itr = itr.next  
+#             itr = itr.next
 
 #         return root.start
 
@@ -51,59 +51,49 @@
 #     def insert_at_begining(self,data) -> None:
 
 #         node = ListNode(data,self.start)
-#         self.start = node 
+#         self.start = node
 
 
-# Method 2 : 
-#   reversing the direction of pointer 
+# Method 2 :
+#   reversing the direction of pointer
 #   given LL = 25 --> 20 --> 12 --> 15 --> 10 --> 5 --> 0
 #   new LL   = 25 <-- 20 <-- 12 <-- 15 <-- 10 <-- 5 <-- 0
 #   the new LL is reversed . this is more efficient
-# two pointer prev - null   curr ptr  - first node 
+# two pointer prev - null   curr ptr  - first node
+
 
 # NEETCODE  solution
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+
 class Solution:
-    
+
     def reverseList(self, head: [ListNode]) -> [ListNode]:
         # prev , curr = None , head
-        
+
         # iteratively  # T - O(n) S - O(1)
         # while curr:
         #     nxt = curr.next
-        #     curr.next = prev 
-        #     prev = curr 
+        #     curr.next = prev
+        #     prev = curr
         #     curr = nxt
-        
+
         # return prev
 
         # Recursive    T - O(n)  S - O(n)
-        
+
         if not head:
             return None
-        
-        newhead = head 
-        
-        if head.next: 
-            newhead = self.reverseList(head.next) 
-            head.next.next = head 
+
+        newhead = head
+
+        if head.next:
+            newhead = self.reverseList(head.next)
+            head.next.next = head
 
         head.next = None
-        
+
         return newhead
-
-
-
-
-
-
-
-
-
-
-
-        
