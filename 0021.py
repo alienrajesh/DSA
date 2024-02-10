04aa9f0342807831aabdb9cf4943a6bf85011c8d
Alien Rajesh <149107155+alienrajesh@users.noreply.github.com>
2024-02-11 00:33:55 +0530

    Merge Two Sorted Linked List

--- 0021.py
+++ 0021.py
@@ -10,4 +10,36 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: [ListNode], list2: [ListNode]) -> [ListNode]:
-        pass 
+        dummy = ListNode()
+        tail = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next 
+            tail = tail.next 
+        
+        if list1:
+            tail.next = list1
+        elif list1:
+            tail.next = list2
+        
+        return dummy.next
+    
+    
+list1 = ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
+list2 = ListNode(2,ListNode(3,ListNode(5,ListNode(8))))
+                
+    
+my_solution = Solution()
+merged_list = my_solution.mergeTwoLists(list1,list2)
+     
+while merged_list:
+    suffix = " --> "
+    if not merged_list.next:
+        suffix = ""
+    print(merged_list.val,end = suffix)
+    merged_list = merged_list.next
