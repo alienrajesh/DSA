37929bec3bb75158f759d51361bd6ff89982ff57
manrajesh <mandalrajeshkumar64@gmail.com>
2024-02-17 17:56:17 +0530

    Remove Nth Node From End

--- 0019.py
+++ 0019.py
@@ -1,18 +1,20 @@
+# leetcode 0019.py Remove N'th node from end 
+
 # Given the head of a linked list,
-# remove the nth node from the end of the list and 
+# remove the nth node from the end of the list and
 # return its head.
 
 # Exmaple 1 :
 # head   = 1 --> 2 --> 3 --> 4 --> 5  , n = 2
-# output = 1 --> 2 --> 3 --> 5 
+# output = 1 --> 2 --> 3 --> 5
 
 # Example 2:
-# head = 1 --> 2 , n = 1 
-# output = 1 
+# head = 1 --> 2 , n = 1
+# output = 1
 
-# Example 3 :
-# head = 1 , n = 1 
-# output = None 
+# Example 3 : only one element in linked list
+# head = 1 , n = 1
+# output = None
 
 
 class ListNode:
@@ -20,20 +22,18 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
     def removeNthFromEnd(self, head: [ListNode], n: int) -> [ListNode]:
-        pass 
+        pass
 
 
-
-
-
-head = ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5)))))
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 my_solution = Solution()
 removed_list = my_solution.removeNthFromEnd(head)
 
 while removed_list:
     suffix = " --> "
-    if  not removed_list.next:
+    if not removed_list.next:
         suffix = ""
-    print(removed_list.val,end=suffix)
+    print(removed_list.val, end=suffix)
