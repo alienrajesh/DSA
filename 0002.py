739ea9320bc2b12355a5f6a7be61db06fe0ecc40
manrajesh <mandalrajeshkumar64@gmail.com>
2024-02-23 19:47:16 +0530

    Add two number of Linked List

--- 0002.py
+++ 0002.py
@@ -30,14 +30,50 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
+
+
 class Solution:
-    def addTwoNumbers(self, l1: [ListNode], l2: [ListNode]) -> [ListNode]:
+   def __init__(self) :
+        self.head = None
+
+   def insert(self,data) :
+        node = ListNode(data,self.head)
+        self.head = node
+
+   def addTwoNumbers(self, l1: [ListNode], l2: [ListNode]) -> [ListNode]:
         
-        added = ListNode(0,None)
-        up = l1
-        down = l2 
+        number1 = ""
+        number2 = ""
+
+        while l1 :
+            number1 += str(l1.val)
+            l1 = l1.next 
+            
+        while l2 :
+            number2 += str(l2.val)
+            l2 = l2.next
         
-        while up and down:
-            pass 
+        number1 = number1[::-1]
+        number2 = number2[::-1]
+
+        result = int(number2) + int(number1)
         
+        for r in str(result):
+            self.insert(int(r))
+            
+        return ListNode 
+# class LinkedList:
+
+l1 = ListNode(2,ListNode(4,ListNode(3)))
+l2 = ListNode(5,ListNode(6,ListNode(4)))
+my_solution = Solution()
+added_numbers = my_solution.addTwoNumbers(l1,l2)
+
+while added_numbers:
+    suffix = " --> "
+    if not added_numbers.next:
+        suffix = ""
+
+    print(added_numbers.val,end = suffix)
+    added_numbers = added_numbers.next
+
