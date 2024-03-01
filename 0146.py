c3762a3338dd559a623f56a6161b5cb08bd9f9af
manrajesh <mandalrajeshkumar64@gmail.com>
2024-03-01 19:56:28 +0530

    Least Recently Used Cache

--- 0146.py
+++ 0146.py
@@ -34,14 +34,55 @@
 # lRUCache.get(3);    // return 3
 # lRUCache.get(4);    // return 4
 
+class Node:
+    def __init__(self,key,value) -> None:
+        self.key = key 
+        self.val = value
+        self.prev = self.next = None
+        
+        
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass        
-
+        self.cache = {} # map key to node
+        self.cap = capacity
+        
+        # left = LRU , right = Most Used 
+        self.left , self.right = Node(0,0) , Node(0,0)
+        self.left.next , self.right.prev = self.right,self.left
+    
+    # remove node from list
+    def remove(self,node) :
+        prev, nxt = node.prev , node.next 
+        prev.next , nxt.prev = nxt , prev 
+        
+    # insert node at right
+    def insert(self,node):
+        prev, nxt = self.right.prev , self.right
+        prev.next = nxt.prev = node 
+        node.next , node.prev = nxt , prev 
+        
     def get(self, key: int) -> int:
-        pass 
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        pass 
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+            
+
+
+        
