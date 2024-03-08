5e1387f483970ae008ddace924de05fd5d1e9c71
manrajesh <mandalrajeshkumar64@gmail.com>
2024-03-08 20:52:06 +0530

    Count Elements With Maximum Frequency

---
+++ 3005.py
@@ -0,0 +1,41 @@
+# 3005. Count Elements With Maximum Frequency
+
+
+# You are given an array nums consisting of positive integers.
+#
+# Return the total frequencies of elements in nums such that those elements all
+# have the maximum frequency.
+#
+# The frequency of an element is the number of occurrences of that element in the
+# array.
+
+# Example 1:
+#
+# Input: nums = [1,2,2,3,1,4]
+# Output: 4
+# Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum
+#             frequency in the array.
+# So the number of elements in the array with maximum frequency is 4.
+# Example 2:
+#
+# Input: nums = [1,2,3,4,5]
+# Output: 5
+# Explanation: All elements of the array have a frequency of 1 which is the maximum.
+# So the number of elements in the array with maximum frequency is 5.
+
+
+def maxFrequencyElements(self, nums: List[int]) -> int:
+    
+    if len(nums) == 1:
+        return 1 
+    
+    nums_freq = {}
+    max_freq = 0 
+
+    for num in nums:
+        nums_freq[num] = 1 + nums_freq.get(num,0)
+        max_freq = max(max_freq,nums_freq[num])
+
+    total = sum(val for val in nums_freq.values() if val == max_freq)
+
+    return total  
