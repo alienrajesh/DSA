0a4b630ac72b87942bb6a96676ea9819185b966a
manrajesh <mandalrajeshkumar64@gmail.com>
2024-03-26 21:33:26 +0530

    First Missing Positive Number

--- 0041.py
+++ 0041.py
@@ -29,14 +29,14 @@
 
 
 def firstMissingPositive(nums: list[int]) -> int:
-    n = len(nums)
-
-    nums_set = set(nums)
-    for i in range(1, n + 1):
-        if i not in nums_set:
-            return i
-
-    return n + 1
+    # n = len(nums)
+    #
+    # nums_set = set(nums)
+    # for i in range(1, n + 1):
+    #     if i not in nums_set:
+    #         return i
+    #
+    # return n + 1
     #
     # nums = set(nums)
     # x = 1
@@ -45,3 +45,23 @@ def firstMissingPositive(nums: list[int]) -> int:
     #     x += 1
     #
     # return x
+
+    # time - O(N) Space - O(N)
+
+    for i in range(len(nums)):
+        if nums[i] < 0:
+            nums[i] = 0
+
+    for i in range(len(nums) + 1):
+        val = abs(nums[i])
+
+        if 1 <= val <= len(nums):
+            if nums[val - 1] > 0:
+                nums[val - 1] *= -1
+            elif nums[val - 1] == 0:
+                nums[val - 1] = -1 * (len(nums) + 1)
+
+    for i in range(1, len(nums) + 1):
+        if nums[i - 1] >= 0:
+            return i
+    return len(nums) + 1
