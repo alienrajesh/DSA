5095b993635cd0399d3245a1fc52ab41b4a2c95a
manrajesh <mandalrajeshkumar64@gmail.com>
2024-03-28 01:06:06 +0530

    Binary Subarray With Sum

--- 0930.py
+++ 0930.py
@@ -3,7 +3,7 @@
 # Given a binary array nums and an integer goal, return the number of non-empty
 # subarrays with a sum goal.
 # A subarray is a contiguous part of the array.
-#  
+#
 #
 # Example 1:
 #
@@ -18,26 +18,27 @@
 # Example 2:
 # Input: nums = [0,0,0,0,0], goal = 0
 # Output: 15
-#  
+#
 #
 # Constraints:
 #
 # 1 <= nums.length <= 3 * 104
 # nums[i] is either 0 or 1.
 # 0 <= goal <= nums.length
-def numSubarraysWithSum( nums: list[int], goal: int) -> int:
-    
+def numSubarraysWithSum(nums: list[int], goal: int) -> int:
+
     def helper(x):
-        if x < 0 :
+        if x < 0:
             return 0
-        res = 0 
+        res = 0
         curr_total = 0
-        l = 0 
+        l = 0
         for r in range(len(nums)):
             curr_total += nums[r]
             while curr_total > x:
                 curr_total -= nums[l]
-                l +=1
-            res += (r-l+1)
+                l += 1
+            res += r - l + 1
         return res
-    return helper(goal) - helper(goal-1)
+
+    return helper(goal) - helper(goal - 1)
