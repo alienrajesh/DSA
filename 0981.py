7964b606c34ff00c4e346c4cbfa31638365f3057
manrajesh <mandalrajeshkumar64@gmail.com>
2024-04-04 12:40:03 +0530

    Time Based Key-Value Store

---
+++ 0981.py
@@ -0,0 +1,54 @@
+# 0981.py Time Based Key-Value Store
+
+
+# Example 1:
+
+# Input
+# ["TimeMap", "set", "get", "get", "set", "get", "get"]
+# [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+# Output
+# [null, null, "bar", "bar", null, "bar2", "bar2"]
+
+# Explanation
+# TimeMap timeMap = new TimeMap();
+# timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+# timeMap.get("foo", 1);         // return "bar"
+# timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+# timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+# timeMap.get("foo", 4);         // return "bar2"
+# timeMap.get("foo", 5);         // return "bar2"
+
+
+from collections import defaultdict
+
+
+class TimeMap:
+
+    def __init__(self):
+        self.map = {}  # key : [[value,time]]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.map:
+            self.map[key] = []
+        self.map[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+
+        values = self.map.get(key, [])
+
+        # binary Search
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+
+obj = TimeMap()
+print(obj.map)
