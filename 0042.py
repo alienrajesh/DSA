6e50c047d4ee4612a71aff5167597289f6323dda
manrajesh <mandalrajeshkumar64@gmail.com>
2024-04-12 13:31:50 +0530

    Trapping Rain Water

--- 0042.py
+++ 0042.py
@@ -4,135 +4,115 @@
 # represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
 # In this case, 6 units of rain water (blue section) are being trapped.
 
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
 # formula = min(l,r) - h[i] = units of trapped water
 # if trapped water = -ve then no water trapped
-# else water water trapped = units in +ve 
+# else water water trapped = units in +ve
+
 
 def trap(height: list[int]) -> int:
-    
+
     max_l = []
     max_i = 0
     for i in range(len(height)):
         max_l.append(max_i)
-        max_i = max(max_i,height[i])
-    
+        max_i = max(max_i, height[i])
+
     max_r = []
     max_i = 0
-    for i in range(len(height)-1,-1,-1):
+    for i in range(len(height) - 1, -1, -1):
         max_r.append(max_i)
-        max_i= max(max_i,height[i])   
-    max_r = max_r[::-1]    
+        max_i = max(max_i, height[i])
+    max_r = max_r[::-1]
 
-    area = 0 
+    area = 0
     for i in range(len(height)):
-        trap = min(max_r[i],max_l[i]) - height[i]
-        if trap < 0 :
-            trap = 0 
+        trap = min(max_r[i], max_l[i]) - height[i]
+        if trap < 0:
+            trap = 0
             area += trap
         else:
             area += trap
     return area
-print(trap(height))
- 
 
-# using pointers 
-     
-def trap2(height:list[int]) -> int:
-    
-    if len(height) == 0 :
+
+print(trap(height))
+
+
+# Method: Pointers
+def trap2(height: list[int]) -> int:
+
+    if len(height) == 0:
         return 0
-    
-    l , r = 0 , len(height)-1
-    
+
+    l, r = 0, len(height) - 1
 
     area = 0
     max_l = height[0]
-    max_r = height[len(height)-1]
-    while l < r :
-        
-        if max_l < max_r :
-            l+=1
-            max_l = max(max_l, height[l]) 
+    max_r = height[len(height) - 1]
+    while l < r:
+
+        if max_l < max_r:
+            l += 1
+            max_l = max(max_l, height[l])
             area += max_l - height[l]
-            
-        else : 
-            r-=1
+
+        else:
+            r -= 1
             max_r = max(max_r, height[r])
             area += max_r - height[r]
     print(area)
-            
-                
-
-trap2(height)       
-        
 
 
-def trap3(height:list[int]) -> int:
-    if len(height) == 0 :
-        return 0
-    
-    l , r = 0 , len(height) -1
-    maxleft , maxright = height[l] , height[r]
-    
-    water  = 0
-    while l < r :
-        
-        if maxleft < maxright:
-            l +=1
-            maxleft = max(maxleft,height[l])
-            water += maxleft - height[l]      
-        else:
-            r -= 1
-            maxright = max(maxright,height[r])
-            water += maxright - height[r]
-    return water     
-    
-print(trap3(height))
-    
-    
-    
-# NEETCODE solution 
-# time - O(n) 
-# space - O(1)
-    
-def trapwater(height:list[int]) -> int:
+trap2(height)
+
+
+def trap3(height: list[int]) -> int:
     if len(height) == 0:
         return 0
-    
-    
-    l = 0 
-    r = len(height) -1
-    
-    maxL = height[l] 
-    maxR = height[r] 
-    
-    res = 0
-    while l < r :
-        if maxL < maxR :
+
+    l, r = 0, len(height) - 1
+    maxleft, maxright = height[l], height[r]
+
+    water = 0
+    while l < r:
+
+        if maxleft < maxright:
             l += 1
-            maxL = max(maxL,height[l]) 
-            res += maxL - height[l]
-        else: 
+            maxleft = max(maxleft, height[l])
+            water += maxleft - height[l]
+        else:
             r -= 1
-            maxR = max(maxR , height[r])
-            res += maxR - height[r] 
+            maxright = max(maxright, height[r])
+            water += maxright - height[r]
+    return water
+
+
+print(trap3(height))
+
+
+# NEETCODE solution
+# time - O(n)
+# space - O(1)
+def trapwater(height: list[int]) -> int:
+    if len(height) == 0:
+        return 0
+
+    l = 0
+    r = len(height) - 1
+
+    maxL = height[l]
+    maxR = height[r]
+
+    res = 0
+    while l < r:
+        if maxL < maxR:
+            l += 1
+            maxL = max(maxL, height[l])
+            res += maxL - height[l]
+        else:
+            r -= 1
+            maxR = max(maxR, height[r])
+            res += maxR - height[r]
     return res
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
